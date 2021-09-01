1c6f5e07e94c734deddd5543f86b633f0bb300e5
Idsarth <idsarthdev19@gmail.com>
2021-08-31 19:24:10 -0600

    configuracion basica del proyecto

---
+++ main.tsx
@@ -0,0 +1,14 @@
+import React, { ReactElement } from 'react'
+import {
+  View
+} from 'react-native'
+
+const Main:React.FC = ():ReactElement => {
+  return (
+    <View>
+
+    </View>
+  )
+}
+
+export default Main
